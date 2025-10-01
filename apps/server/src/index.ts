5c8620432990e91f46f70739bb9239a07408b588
Kalpesh Borde <bordekalpesh@gmail.com>
2025-10-01 23:34:46 +0530

    added login/signup routes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,29 @@
 
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/auth.route';
+
+
+// Load environment variables
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Simple route for testing
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Welcome to Madhoor Pureline Backend API!');
 });
+
+// Auth Routes
+app.use('/api/auth', authRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
