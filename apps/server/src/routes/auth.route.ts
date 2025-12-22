1eea6f4eee9b9f2e3322b1a3d811be77f7714437
Kalpesh Borde <bordekalpesh@gmail.com>
2025-12-22 23:16:18 +0530

    feat: enhance authentication routes by adding logout functionality and restrict product creation to admins

--- apps/server/src/routes/auth.route.ts
+++ apps/server/src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUser, login, signup } from '../controllers/auth.controller';
+import { getUser, login, logOutUser, signup } from '../controllers/auth.controller';
 import { protect } from '../middleware/auth';
 
 
@@ -8,7 +8,7 @@ const authRoutes: Router = Router();
 authRoutes.post('/signup', signup);
 authRoutes.post('/login', login);
 authRoutes.get('/me', protect, getUser);
-// authRoutes.get('logout', logOutUser);
+authRoutes.get('logout', logOutUser);
 // authRoutes.post("/users/me/addresses", protect, addUserAddresses);
 // authRoutes.get("/users/me/addresses", protect, getUserAddresses);
 
